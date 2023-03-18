2bb1908a7db7e6ef9f3c0673efec8041a8226735
Wanuja Ranasinghe <63337783+Wanuja97@users.noreply.github.com>
2023-03-18 12:58:52 +0530

    cors installed

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require('express'); 
-const app = express();
-const dotenv= require('dotenv');
-dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
-const port = process.env.PORT;                 
+const app = express();
+dotenv.config();
+const port = process.env.PORT;
+
+// const options = {
+//     origin: '',
+//     methods: 'GET, PUT',
+//   }
+
+// app.use(cors(options));
+
+app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {  
-    res.send('Hello World !!!');  
+
+// test route
+app.get('/', (req, res) => {
+    res.send('Hello World !!!');
 });
 
-app.listen(port, () => {       
-    console.log(`Server listening on port ${port}`); 
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
 });
